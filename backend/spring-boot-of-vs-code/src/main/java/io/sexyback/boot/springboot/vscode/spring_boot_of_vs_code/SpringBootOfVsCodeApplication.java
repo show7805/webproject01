be63b1870c46c38e6aed688ac83d0b2a79a75e00
백승민 <backsm2002@naver.com>
2024-07-18 17:35:05 +0900

    Backend settings

---
+++ backend/spring-boot-of-vs-code/src/main/java/io/sexyback/boot/springboot/vscode/spring_boot_of_vs_code/SpringBootOfVsCodeApplication.java
@@ -0,0 +1,13 @@
+package io.sexyback.boot.springboot.vscode.spring_boot_of_vs_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOfVsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOfVsCodeApplication.class, args);
+	}
+
+}
---
+++ backend/spring-boot-of-vs-code/src/test/java/io/sexyback/boot/springboot/vscode/spring_boot_of_vs_code/SpringBootOfVsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sexyback.boot.springboot.vscode.spring_boot_of_vs_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOfVsCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
